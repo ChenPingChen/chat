1f06fd6fb3e11662924a065bf124dd1066f6bb61
ChenPingChen <jack76483636@gmail.com>
2024-12-09 10:25:16 +0800

    12/9 update batch_import

--- chat/data_manager/urls.py
+++ chat/data_manager/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('data-manager/', views.process_violation_data, name='process_violation'),
+    path('data-manager/batch/', views.batch_process_violations, name='batch_process_violations'),
 ]
